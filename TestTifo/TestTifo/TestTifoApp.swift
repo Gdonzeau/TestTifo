d946348899bc0bacdfea70b4ad81318a2d3bee94
Guillaume Donzeau <60357756+Gdonzeau@users.noreply.github.com>
2022-05-24 16:22:13 +0200

    First commit

---
+++ TestTifo/TestTifo/TestTifoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestTifoApp.swift
+//  TestTifo
+//
+//  Created by Guillaume on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct TestTifoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
